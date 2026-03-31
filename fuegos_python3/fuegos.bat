8d1a848fd558a4193e572516b0dcba936d9c0871
Edwin Piragauta <edwin.piragauta@gmail.com>
2026-03-30 21:20:04 -0500

    Actualización de archivos

--- fuegos_python3/fuegos.bat
+++ fuegos_python3/fuegos.bat
@@ -1,6 +1,1 @@
-@echo off
-REM Script para ejecutar Fuegos.py con Python de ArcGIS Pro 3.x
-REM Migrado de Python 2.7 (ArcGIS Desktop 10.8) a Python 3 (ArcGIS Pro)
-
-cd /d "C:\ws\sinchi\ws\fuegos_python3"
-"C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" "C:\ws\sinchi\ws\fuegos_python3\Fuegos.py"
+"C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" "C:\tmp\fuegos_python3\Fuegos.py"
---
+++ fuegos_python3/fuegos_enviar_correo.bat
@@ -0,0 +1,5 @@
+@echo off
+REM Script para ejecutar Enviar_Email_Fuegos.py 
+
+
+"C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" "C:\tmp\fuegos_python3\Enviar_Email_Fuegos.py"
